52cc2409c6e6523e8895404c17b6a71f5a621f0a
n4rk <m.hachmi_etu@enset-media.ac.ma>
2022-11-06 16:01:37 +0100

    added eureka service

---
+++ eureka-service/src/test/java/com/devhcm/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhcm.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
